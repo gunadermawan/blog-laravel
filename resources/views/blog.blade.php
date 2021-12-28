4826f0e2e7111ac8fbb7e33cddfb50bed84dfcd3
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-12-28 17:13:38 +0700

    make slug to post blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.main')
 @section('container')
     @foreach($blogs as $blog)
-    <h2>{{ $blog["title"] }}</h2>
+    <h2><a href="/blog/{{ $blog['slug'] }}">
+        {{ $blog["title"] }}
+    </a></h2>
     <h5>Oleh: {{ $blog["author"] }}</h5>
     <p>{{ $blog["body"] }}</p>
     @endforeach
--- routes/web.php
+++ routes/web.php
@@ -31,11 +31,13 @@ Route::get('/blog', function () {
     $blog_posts = [
     [
         "title" => "Kotlin",
+        "slug" => "kotlin",
         "author" => "Guna Dermawan",
         "body" => "  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, voluptatum dignissimos. Molestiae expedita numquam illum possimus veritatis, blanditiis laboriosam aliquid amet similique quas provident hic ex qui sapiente rerum ea totam libero! Corrupti vero voluptas quidem voluptate suscipit provident impedit esse distinctio inventore magnam, nemo molestias quae ipsum dicta fugit culpa nesciunt. Id laudantium commodi, corporis cumque, corrupti totam itaque cum officia molestiae, beatae unde dolores rem ratione praesentium soluta pariatur iure doloremque est. Pariatur molestiae vel debitis illo beatae?"
     ],
     [
         "title" => "Kotlin android",
+        "slug" => "kotlin-android",
         "author" => "Guna Dermawan",
         "body" => " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate, voluptatum dignissimos. Molestiae expedita numquam illum possimus veritatis, blanditiis laboriosam aliquid amet similique quas provident hic ex qui sapiente rerum ea totam libero! Corrupti vero voluptas quidem voluptate suscipit provident impedit esse distinctio inventore magnam, nemo molestias quae ipsum dicta fugit culpa nesciunt. Id laudantium commodi, corporis cumque, corrupti totam itaque cum officia molestiae, beatae unde dolores rem ratione praesentium soluta pariatur iure doloremque est. Pariatur molestiae vel debitis illo beatae? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate"
     ]
