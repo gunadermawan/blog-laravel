9bd1a4303d6fd48b8d24324a561ca15972866ef7
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-01-03 11:45:57 +0700

    improve ui post

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -5,9 +5,10 @@
     @if ($blogs->count())
         <div class="card mb-3">
             <img src="https://source.unsplash.com/1200x400?{{ $blogs[0]->category->name }}" class="card-img-top"
-                alt="...">
+                alt="{{ $blogs[0]->category->name }}">
             <div class="card-body text-center">
-                <h3 class="card-title">{{ $blogs[0]->title }}</h3>
+                <h3 class="card-title"><a href="/blog/{{ $blogs[0]->slug }}"
+                        class="text-decoration-none text-dark">{{ $blogs[0]->title }}</a></h3>
                 <small class="text-muted">
                     <p>Oleh: <a href="#" class="text-decoration-none">{{ $blogs[0]->user->name }}</a>
                         Kategori: <a href="/categories/{{ $blogs[0]->category->slug }}"
@@ -20,20 +21,32 @@
         </div>
     @else
         <p class="text-center fs-4">Not Post Found!</p>
-
     @endif
 
-    @foreach ($blogs as $blog)
-        <article class="mb-5 border-bottom pb-4">
-            <h2><a href="/blog/{{ $blog->slug }}" class="text-decoration-none">
-                    {{ $blog->title }}
-                </a></h2>
-            <p>Oleh: <a href="#" class="text-decoration-none">{{ $blog->user->name }}</a>
-            </p>
-            <p>Kategori: <a href="/categories/{{ $blog->category->slug }}"
-                    class="text-decoration-none">{{ $blog->category->name }}</a></p>
-            <p>{{ $blog->exerpt }}</p>
-            <a href="/blog/{{ $blog->slug }}" class="text-decoration-none">Read more...</a>
-        </article>
-    @endforeach
+    <div class="container">
+        <div class="row">
+            @foreach ($blogs as $blog)
+                <div class="col-md-4">
+                    <div class="card mb-3">
+                        <div class="position-absolute bg-dark px-3 py-2 text-white"
+                            style="background-color: rgba(0, 0, 0, 0.5)"><a
+                                href="/categories/{{ $blogs[0]->category->slug }}"
+                                class="text-decoration-none text-white">{{ $blog->category->name }}</a>
+                        </div>
+                        <img src=" https://source.unsplash.com/500x400?{{ $blog->category->name }}"
+                            class="card-img-top" alt="{{ $blog->category->name }}">
+                        <div class="card-body">
+                            <h5 class="card-title"> {{ $blog->title }}</h5>
+                            <small class="text-muted">
+                                <p>Oleh: <a href="#" class="text-decoration-none">{{ $blog->user->name }}</a>
+                                    updated on: {{ $blog->created_at->diffForHumans() }} </p>
+                            </small>
+                            <p class="card-text">{{ $blog->exerpt }}</p>
+                            <a href="/blog/{{ $blog->slug }}" class="btn btn-primary">Read more</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
