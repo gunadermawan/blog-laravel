7edf1c99f009e59b4ad0f6a97201af61dc39e35f
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-01-03 16:51:56 +0700

    update syntax post models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,10 +14,16 @@ class Post extends Model
 
     public function scopeFilter($query, array $filters)
     {
-        if (isset($filters['search']) ? $filters['search'] : false) {
-            return $query->where('title', 'like', '%' . $filters['search'] . '%')
-                ->orWhere('body', 'like', '%' . $filters['search'] . '%');
-        }
+        // if (isset($filters['search']) ? $filters['search'] : false) {
+        //     return $query->where('title', 'like', '%' . $filters['search'] . '%')
+        //         ->orWhere('body', 'like', '%' . $filters['search'] . '%');
+        // }
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            return $query->where(function ($query) use ($search) {
+                $query->where('title', 'like', '%' . $search . '%')
+                    ->orWhere('body', 'like', '%' . $search . '%');
+            });
+        });
     }
 
     public function category()
