250a212308323478d5ec1dda6e199d9c5f1722f5
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-01-03 13:18:17 +0700

    chnage ui about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>About social coding</h1>
-    <h3>{{ $nama }}</h3>
-    <p>{{ $email }}</p>
-    <img src="img/{{ $image }}" alt={{ $nama }} width="200" class="img-thumbnail rounded-circle">
+    <div class="text-center">
+        <h1>About social coding</h1>
+        <h3>{{ $nama }}</h3>
+        <p>{{ $email }}</p>
+        <img src="img/{{ $image }}" alt={{ $nama }} width="200" class="img-thumbnail rounded">
+    </div>
 @endsection
