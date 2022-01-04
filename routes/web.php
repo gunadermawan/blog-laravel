ee290131ad54a717bbfad9565d588699fc08403b
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-01-04 17:28:20 +0700

    improve access page dashboard and rename route

--- routes/web.php
+++ routes/web.php
@@ -63,8 +63,9 @@ Route::get('/authors/{author:username}', function (User $author) {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index'])->middleware('guest');
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
-Route::get('/register', [RegisterController::class, 'index']);
+Route::post('/logout', [LoginController::class, 'logout']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store']);
-Route::get('/dashboard', [DashboardController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
