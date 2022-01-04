96e99b95f871cef5d8161faecb9bf04171bfe9cf
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-01-04 16:52:44 +0700

    membuat route login

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -12,15 +12,21 @@
                     </div>
                 @endif
                 <form action="/login" method="POST">
+                    @csrf
                     <div class="form-floating">
-                        <input type="email" name="email" class="form-control @error('email')is-invalid @enderror"
-                            id="floatingInput" placeholder="name@example.com">
-                        <label for="floatingInput">Email address</label>
+                        <input type="email" name="email" class="form-control @error('email')is-invalid @enderror" id="email"
+                            placeholder="name@example.com" autofocus required value="{{ old('email') }}">
+                        <label for="email">Email address</label>
+                        @error('email')
+                            <div class="invalid-feedback">
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
                         <input type="password" name="password" class="form-control @error('password')is-invalid @enderror"
-                            id="floatingPassword" placeholder="Password">
-                        <label for="floatingPassword">Password</label>
+                            id="password" placeholder="Password" required>
+                        <label for="Password">Password</label>
                     </div>
                     <button class="w-100 btn btn-lg btn-dark" type="submit">Login</button>
                 </form>
