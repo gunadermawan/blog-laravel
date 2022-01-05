92eb766397d188c71e280b5fc063f6e8e7e73cdf
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-01-05 21:44:04 +0700

    make funtion update data

--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -5,7 +5,8 @@
             <div class="col-lg-8">
                 <h2 class="mb-3">{{ $post->title }}</h2>
                 <a href="/dashboard/posts" class="btn btn-dark"><span data-feather="arrow-left"></span> Back</a>
-                <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span>
+                    Edit</a>
                 <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
                     @method('delete')
                     @csrf
