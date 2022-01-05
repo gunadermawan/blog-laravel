fa5f1cd507737db0bc6422a960e5e1f6fb5fca22
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-01-05 17:27:22 +0700

    improve ui

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -6,7 +6,7 @@
     <div class="table-responsive">
         <a href="/dashboard/posts/create" class="btn btn-outline-primary">New Post</a>
         @if (session()->has('success'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
                 {{ session('success') }}
                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
--- resources/views/dashboard/posts/show.blade.php
+++ resources/views/dashboard/posts/show.blade.php
@@ -12,7 +12,6 @@
                 <article class="my-3 fs-5">
                     {!! $post->body !!}
                 </article>
-                <a href="/blog" class="mt-3 ">Back</a>
             </div>
         </div>
     </div>
