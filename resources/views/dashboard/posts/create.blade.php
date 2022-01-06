6a1ffda050c50d02124ef89246ee8357c615668b
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-01-06 13:15:40 +0700

    showing preview img on upload post

--- resources/views/dashboard/posts/create.blade.php
+++ resources/views/dashboard/posts/create.blade.php
@@ -40,7 +40,9 @@
             </div>
             <div class="mb-3">
                 <label for="image" class="form-label">Post Image</label>
-                <input class="form-control @error('image') is-invalid @enderror " type="file" id="image" name="image">
+                <img src="" class="img-preview img-fluid mb-3 col-sm-5" alt="">
+                <input class="form-control @error('image') is-invalid @enderror " type="file" id="image" name="image"
+                    onchange="previewImage()">
                 @error('image')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -69,5 +71,16 @@
         document.addEventListener('trix-file-accept', function(e) {
             e.preventDefault();
         })
+
+        function previewImage() {
+            const image = document.querySelector('#image');
+            const imgPreview = document.querySelector('.img-preview');
+            imgPreview.style.display = 'block';
+            const oFReader = new FileReader();
+            oFReader.readAsDataURL(image.files[0]);
+            oFReader.onload = function(OFREvent) {
+                imgPreview.src = OFREvent.target.result;
+            }
+        }
     </script>
 @endsection
