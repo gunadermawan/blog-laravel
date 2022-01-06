df6fc801949952d90b5e3f23f4d2e7b4127a283c
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-01-06 11:20:28 +0700

    function upload image to db

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -43,13 +43,16 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        return $request->file('image')->store('post-images');
         $validatedData = $request->validate([
             'title' => 'required|max:255',
             'slug' => 'required|unique:posts',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ]);
+        if ($request->file('image')) {
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['exerpt'] = Str::limit(strip_tags($request->body), 200);
         Post::create($validatedData);
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,11 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // User::create([
-        //     'name' => 'Guna Dermawan',
-        //     'email' => 'gunadermawan@gmail.com',
-        //     'password' => bcrypt('12345')
-        // ]);
+        User::create([
+            'name' => 'Guna Dermawan',
+            'username' => 'gunadermawan',
+            'email' => 'gunadermawan@gmail.com',
+            'password' => bcrypt('password')
+        ]);
 
         // User::create([
         //     'name' => 'zezee',
