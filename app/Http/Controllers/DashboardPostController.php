758b9f03fa071f9cb4c71c36ca89865fb7be13aa
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2022-01-06 13:34:32 +0700

    make fun delete img from db and file resources

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -7,7 +7,7 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
-
+use Illuminate\Support\Facades\Storage;
 
 class DashboardPostController extends Controller
 {
@@ -98,12 +98,23 @@ class DashboardPostController extends Controller
         $rules = [
             'title' => 'required|max:255',
             'category_id' => 'required',
+            'image' => 'image|file|max:1024',
             'body' => 'required'
         ];
+
         if ($request->slug != $post->slug) {
             $rules['slug'] = 'required|unique:posts';
         }
+
         $validatedData = $request->validate($rules);
+
+        if ($request->file('image')) {
+            if ($request->oldImage) {
+                Storage::delete($request->oldImage);
+            }
+            $validatedData['image'] = $request->file('image')->store('post-images');
+        }
+
         $validatedData['user_id'] = auth()->user()->id;
         $validatedData['exerpt'] = Str::limit(strip_tags($request->body), 200);
         Post::where('id', $post->id)
@@ -119,6 +130,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
+        if ($post->image) {
+            Storage::delete($post->image);
+        }
         Post::destroy($post->id);
         return redirect('/dashboard/posts')->with('success', 'post has been deleted!');
     }
--- resources/views/dashboard/posts/edit.blade.php
+++ resources/views/dashboard/posts/edit.blade.php
@@ -5,7 +5,8 @@
         <h1 class="h2">Edit Post</h1>
     </div>
     <div class="col-lg-8">
-        <form method="POST" action="/dashboard/posts/{{ $post->slug }}" class="mb-5">
+        <form method="POST" action="/dashboard/posts/{{ $post->slug }}" class="mb-5"
+            enctype="multipart/form-data">
             @method('put')
             @csrf
             <div class="mb-3">
@@ -41,6 +42,23 @@
                 </select>
             </div>
             <div class="mb-3">
+                <label for="image" class="form-label">Post Image</label>
+                <input type="hidden" name="oldImage" value="{{ $post->image }}">
+                @if ($post->image)
+                    <img src="{{ asset('storage/' . $post->image) }}" class="img-preview img-fluid mb-3 col-sm-5 d-block"
+                        alt="">
+                @else
+                    <img src="" class="img-preview img-fluid mb-3 col-sm-5" alt="">
+                @endif
+                <input class="form-control @error('image') is-invalid @enderror " type="file" id="image" name="image"
+                    onchange="previewImage()">
+                @error('image')
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
+                @enderror
+            </div>
+            <div class="mb-3">
                 <label for="body" class="form-label">Body</label>
                 @error('body')
                     <p class="text-danger">{{ $message }}</p>
@@ -62,5 +80,16 @@
         document.addEventListener('trix-file-accept', function(e) {
             e.preventDefault();
         })
+
+        function previewImage() {
+            const image = document.querySelector('#image');
+            const imgPreview = document.querySelector('.img-preview');
+            imgPreview.style.display = 'block';
+            const oFReader = new FileReader();
+            oFReader.readAsDataURL(image.files[0]);
+            oFReader.onload = function(OFREvent) {
+                imgPreview.src = OFREvent.target.result;
+            }
+        }
     </script>
 @endsection
